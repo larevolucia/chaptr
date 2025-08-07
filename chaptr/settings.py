d91996da2c1542f2a376dad874eea39b50a5870d
larevolucia <reis.lucia@gmail.com>
2025-08-07 14:15:04 +0200

    Move variables to .env file

--- chaptr/settings.py
+++ chaptr/settings.py
@@ -86,14 +86,14 @@ WSGI_APPLICATION = 'chaptr.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
-DATABASES = {'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))}
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',  # fallback for local dev
+        conn_max_age=600
+    )
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
