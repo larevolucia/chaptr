47c148d75ef934caade3098244bbceb83b1369d1
larevolucia <reis.lucia@gmail.com>
2025-08-12 13:15:26 +0200

    Create book detail view and url

--- books/urls.py
+++ books/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),
-    path('search/', views.book_search, name='book-search')
+    path('search/', views.book_search, name='book-search'),
+    path("books/<str:book_id>/", views.book_detail, name="book_detail"),
 ]
