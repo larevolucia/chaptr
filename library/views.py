6287ec074783bf9c63a8121d5bffc8587eee25d1
larevolucia <reis.lucia@gmail.com>
2025-08-26 12:13:22 +0200

    Add query on books per reading status and display on template

--- library/views.py
+++ library/views.py
@@ -1,8 +1,37 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
+from books.models import Book
+from activity.models import ReadingStatus
 
 # Create your views here.
 
 @login_required
 def library(request):
-    return render(request, "library/library.html")
+    # These three queries are easy to read and reason about:
+    to_read = (
+        Book.objects
+        .filter(reading_statuses__user=request.user,
+                reading_statuses__status=ReadingStatus.Status.TO_READ)
+        .only("id", "title", "thumbnail_url")         # load just what you need
+        .distinct()
+    )
+    reading = (
+        Book.objects
+        .filter(reading_statuses__user=request.user,
+                reading_statuses__status=ReadingStatus.Status.READING)
+        .only("id", "title", "thumbnail_url")
+        .distinct()
+    )
+    read = (
+        Book.objects
+        .filter(reading_statuses__user=request.user,
+                reading_statuses__status=ReadingStatus.Status.READ)
+        .only("id", "title", "thumbnail_url")
+        .distinct()
+    )
+
+    return render(
+        request,
+        "library/library.html",
+        {"to_read": to_read, "reading": reading, "read": read}
+        )
