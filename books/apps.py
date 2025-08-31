cc823b2f22abd6d29098c03f7c28d8bfcc670e5f
larevolucia <reis.lucia@gmail.com>
2025-08-31 17:50:40 +0200

    Remove signals

--- books/apps.py
+++ books/apps.py
@@ -5,6 +5,3 @@ class BooksConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'books'
 
-    def ready(self):
-        # Register signal handlers
-        import books.signals  
